3822f977dffcecb9cea9b0d758832a2f33dd24b8
Avengerandy007 <Avengerandy007@users.noreply.github.com>
2025-07-14 21:44:52 +0300

    Render empty window

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program{
+	public static void Main(){
+		Console.WriteLine("Hello from bad snake clone");
+		Window.Setup();
+		Window.MainLoop();
+	}
+}
---
+++ Window.cs
@@ -0,0 +1,88 @@
+using static SDL2.SDL;
+
+static class Window{
+	
+	public static IntPtr renderer;
+	static IntPtr window;
+
+	static bool running;
+
+	public static void Setup(){
+		
+		running = true;
+
+		if (InitSDL() == -1){
+			InitSDL();
+		}
+
+		if (InitWindow() == -1){
+			InitWindow();
+		}
+
+		if (InitRenderer() == -1){
+			InitRenderer();
+		}
+		
+	}
+
+	static int InitSDL(){
+		if (SDL_Init(SDL_INIT_VIDEO) < 0){
+			Console.WriteLine($"There was a problem initialising SDL: {SDL_GetError()}, retrying.");
+			return -1;
+		}
+		return 0;
+	}
+
+	static int InitWindow(){
+		window = SDL_CreateWindow("Not well made clone of snake", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WindowFlags.SDL_WINDOW_SHOWN);
+
+		if (window == IntPtr.Zero){
+			Console.WriteLine($"There was a problem creating the window: {SDL_GetError()}, retrying.");
+			return -1;
+		}
+
+		return 0;
+	}
+
+	static int InitRenderer(){
+		renderer = SDL_CreateRenderer(window, -1, SDL_RendererFlags.SDL_RENDERER_ACCELERATED);
+
+		if (renderer == IntPtr.Zero){
+			Console.WriteLine($"There was a problem creating the renderer: {SDL_GetError()}, retrying.");
+			return -1;
+		}
+
+		return 0;
+	}
+
+	public static void MainLoop(){
+		while (running){
+			Render();
+			PollEvents();
+		}
+		DestroySDLElements();
+	}
+
+	static void Render(){
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_RenderClear(renderer);
+		//TODO: ADD ALL OBJECT SPECIFIC RENDERING LOGIC FUNCTIONS
+		SDL_RenderPresent(renderer);
+	}
+
+	static void PollEvents(){
+		while(SDL_PollEvent(out SDL_Event e) == 1){
+			switch (e.type){
+				case SDL_EventType.SDL_QUIT:
+					running = false;
+					break;
+			}
+		}
+	}
+
+	static void DestroySDLElements(){
+		SDL_DestroyWindow(window);
+		SDL_DestroyRenderer(renderer);
+		SDL_Quit();
+	}
+}
